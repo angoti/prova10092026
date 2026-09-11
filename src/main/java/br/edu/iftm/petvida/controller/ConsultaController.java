4f9cdd660da2b4f78ae1f6c3c19f6de18d087cd7
Edson Angoti Junior <angoti@iftm.edu.br>
2026-09-10 22:09:49 -0300

    parte-d: defeitos corrigidos

--- src/main/java/br/edu/iftm/petvida/controller/ConsultaController.java
+++ src/main/java/br/edu/iftm/petvida/controller/ConsultaController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import br.edu.iftm.petvida.repository.AnimalRepository;
 
+@Controller
 public class ConsultaController { // <== ITEM 1
 
   @Autowired
@@ -13,7 +14,7 @@ public class ConsultaController { // <== ITEM 1
 
   @GetMapping("consulta") // <== ITEM 2
   public String consulta(Model model) {
-    model.addAttribute("bicho", repository.buscarPorId(1)); // <== ITEM 3
+    model.addAttribute("animal", repository.buscarPorId(2)); // <== ITEM 3
     return "consulta.html"; // <== ITEM 4
   }
 }
